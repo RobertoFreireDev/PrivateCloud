c7121c237357bbfde61de7686214d27891215c30
roberto freire <robertofreireferrazpassos@gmail.com>
2025-11-20 08:33:37 -0300

    create JsEditor

---
+++ Portal/Components/JsEditor.razor.cs
@@ -0,0 +1,18 @@
+﻿namespace Portal.Components;
+
+public partial class JsEditor : ComponentBase
+{
+    [Parameter]
+    public string CodeContent { get; set; }
+
+    [Inject]
+    public IJSRuntime JS { get; set; }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            await JS.InvokeVoidAsync("AceEditorInit");
+        }
+    }
+}
