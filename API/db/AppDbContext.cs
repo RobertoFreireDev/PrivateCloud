84996b7dcedacd5224a857c0afc4817972e1218d
roberto freire <robertofreireferrazpassos@gmail.com>
2025-11-20 15:32:44 -0300

    Make Name unique

--- API/db/AppDbContext.cs
+++ API/db/AppDbContext.cs
@@ -9,4 +9,23 @@ public class AppDbContext : DbContext
 
     public DbSet<FileEntity> Files { get; set; }
     public DbSet<PageEntity> Pages { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<FileEntity>()
+            .HasKey(f => f.Id);
+
+        modelBuilder.Entity<FileEntity>()
+            .HasIndex(f => f.Name)
+            .IsUnique();
+
+        modelBuilder.Entity<PageEntity>()
+            .HasKey(f => f.Id);
+
+        modelBuilder.Entity<PageEntity>()
+            .HasIndex(f => f.Name)
+            .IsUnique();
+    }
 }
